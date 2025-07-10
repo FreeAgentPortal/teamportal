50d7c912c674cfba8bd20725928220dc4a5ff657
Austin Howard <cnfishead@hotmail.com>
2025-07-10 15:01:14 -0400

    dynamic reports page work

--- src/app/opportunities_hub/reports/page.tsx
+++ src/app/opportunities_hub/reports/page.tsx
@@ -1,20 +1,18 @@
 import { navigation } from '@/data/navigation';
 import PageLayout from '@/layout/page/Page.layout';
+import Report from '@/views/opportunity_hub/report/Report.view';
 import { Metadata } from 'next';
-
 
 export const metadata: Metadata = {
   title: 'Dynamic Reports',
   description: 'This page will allow users to view and manage their reports dynamically.',
   keywords: ['reports', 'dynamic reports', 'opportunities hub'],
-}
+};
 
 export default function Page() {
   return (
     <PageLayout pages={[navigation().opportunities_hub.links.reports]} largeSideBar>
-      <h1>Dynamic Reports</h1>
-      <p>This page will allow users to view and manage their reports dynamically.</p>
-      {/* Add your dynamic reports components here */}
+      <Report />
     </PageLayout>
   );
 }
---
+++ src/views/opportunity_hub/report/Report.view.tsx
@@ -0,0 +1,110 @@
+'use client';
+import React from 'react';
+import styles from './Report.module.scss';
+import { useQueryClient } from '@tanstack/react-query';
+import SearchWrapper from '@/layout/searchWrapper/SearchWrapper.layout';
+import { FaEye, FaEyeSlash, FaPlus } from 'react-icons/fa';
+import useApiHook from '@/hooks/useApi';
+import { Button, Table, Tooltip } from 'antd';
+
+const Report = () => {
+  const queryClient = useQueryClient();
+  const selectedProfile = queryClient.getQueryData(['profile', 'team']) as any;
+
+  const { data, isLoading, isFetching } = useApiHook({
+    url: '/search-preference/report',
+    method: 'GET',
+    filter: `ownerId;${selectedProfile?.payload?._id}`,
+    key: 'reports',
+    enabled: !!selectedProfile?.payload?._id,
+  }) as any;
+
+  const { mutate: update } = useApiHook({
+    method: 'PUT',
+    key: 'update_report',
+    queriesToInvalidate: ['reports'],
+  }) as any;
+
+  return (
+    <SearchWrapper
+      placeholder="Filter reports"
+      queryKey="reports"
+      buttons={[]}
+      filters={[
+        {
+          key: '',
+          label: 'All',
+        },
+        {
+          key: 'opened;true',
+          label: 'Viewed',
+        },
+        {
+          key: 'opened;false',
+          label: 'Not Viewed',
+        },
+      ]}
+      sort={[
+        {
+          key: '',
+          label: 'Default',
+        },
+      ]}
+      total={data?.payload?.totalCount}
+      isFetching={isFetching}
+    >
+      <Table
+        dataSource={data?.payload}
+        loading={isLoading}
+        pagination={false}
+        rowKey={(record: any) => record._id}
+        columns={[
+          {
+            title: 'Report Generated For',
+            // This should be the name of the search preference, searchPreference.name
+            key: 'reportId',
+            render: (_, record) => <span style={{ maxWidth: '100px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{record.searchPreference.name}</span>,
+          },
+          {
+            title: '# of Opportunities',
+            dataIndex: 'results',
+            key: 'results',
+            render: (text) => <span>{text?.length || 0}</span>,
+          },
+          {
+            title: "Viewed",
+            dataIndex: 'opened',
+            key: 'opened',
+            render: (_, record) => (
+              <span>{record.opened ? 'Yes' : 'No'}</span>
+            ),
+          },
+          {
+            title: 'Actions',
+            dataIndex: 'actions',
+            key: 'actions',
+            render: (text, record) => (
+              <div style={{ display: 'flex', gap: '10px' }}>
+                <Tooltip title={!record.opened ? 'Mark as Viewed' : 'Mark as Not Viewed'}>
+                  <Button
+                    onClick={() => {
+                      update({
+                        url: `/search-preference/report/${record._id}`,
+                        formData: { opened: !record.opened },
+                      });
+                    }}
+                  >
+                    {/* Switches between a view and viewed icon */}
+                    {!record.opened ? <FaEye /> : <FaEyeSlash />}
+                  </Button>
+                </Tooltip>
+              </div>
+            ),
+          },
+        ]}
+      />
+    </SearchWrapper>
+  );
+};
+
+export default Report;
